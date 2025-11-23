148afeb41d3b83a44c0ab9f2bfd09ff778b4ad80
frostdev-ops <admin@frostdev.io>
2025-11-23 01:19:09 -0500

    feat: add launcher release routes to admin app

--- wowid3-server/web/src/pages/LauncherReleasesList.tsx
+++ wowid3-server/web/src/pages/LauncherReleasesList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { api } from '../api/client';
+import api from '../api/client';
 
 interface LauncherVersion {
   version: string;
